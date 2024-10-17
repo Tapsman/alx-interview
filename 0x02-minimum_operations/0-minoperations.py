740c47c212b570adb5a00c37357193e3f331949c
Tapsman <tapiwanyoni140@gmail.com>
2024-10-17 01:14:58 -0700

    Minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Minimum Operations
+"""
+
+def minOperations(n):
+    """
+    This is a function that can execute only two operations
+    Then copy's all and pastes them
+    """
+    if n <= 1:
+        return 0
+
+    operands = 0
+    div = 2
+
+    while n > 1:
+        while n % div == 0:
+            operands = operands + div
+            n //= div
+        div = div + 1
+
+    return operands
