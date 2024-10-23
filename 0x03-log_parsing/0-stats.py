3ca1073e62c0322f1dc7a9b8eb4bd1b523368404
Tapsman <tapiwanyoni140@gmail.com>
2024-10-23 10:18:39 -0700

    This is my commit message for the log passing interview

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+"""
+This is a script that is going to read the std input
+line after line
+"""
+
+import sys
+
+
+def printRank(dic, size):
+    """
+    The function will then output the information
+    """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
+
+
+rankcode = {"200": 0,
+            "301": 0,
+            "400": 0,
+            "401": 0,
+            "403": 0,
+            "404": 0,
+            "405": 0,
+            "500": 0}
+
+
+compute = 0
+size = 0
+
+try:
+    for line in sys.stdin:
+        if compute != 0 and compute % 10 == 0:
+            printRank(rankcode, size)
+
+        stlist = line.split()
+        compute = compute + 1
+
+        try:
+            size = size + int(stlist[-1])
+        except Exception:
+            pass
+
+        try:
+            if stlist[-2] in rankcode:
+                rankcode[stlist[-2]] += 1
+        except Exception:
+            pass
+        printRank(rankcode, size)
+
+
+except KeyboardInterrupt:
+    printRank(rankcode, size)
+    raise
---
+++ 0x03-log_parsing/README.md
@@ -0,0 +1,1 @@
+This is my readme for the logg_parsing interview code
